10320912a7a456099094c6d3f760177ef8d173b1
ShadowsSide <61958350+ShadowsSide@users.noreply.github.com>
2022-05-23 23:59:17 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,129 +1,132 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Hezu Wiki",
-  tagline: "欢迎加入电报第一合租群",
-  url: "https://hezu.wiki",
-  baseUrl: "https://www.hezu.wiki/docs/About_Us",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/Doraemon.svg",
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ShadowsSide ", // Usually your GitHub org/user name.
-  projectName: "hezu-wiki", // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  
-
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      })
-    ]
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "电报第一合租群@hezu1",
+        title: 'My Site',
         logo: {
-          alt: "My Site Logo",
-          src: "img/Doraemon.svg"
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
         },
         items: [
           {
-            type: "doc",
-            docId: "About_Us",
-            position: "left",
-            label: "群组介绍及群规"
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
           },
-
-          /**隐藏blog */
-          /**{ to: "/blog", label: "Blog", position: "left" },*/
-
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: "https://t.me/hezu1",
-            label: "加入我们",
-            position: "right"
-          }
-        ]
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "群组介绍",
+            title: 'Docs',
             items: [
               {
-                label: "关于我们",
-                to: "/docs/About_Us"
-              }
-            ]
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
           },
           {
-            title: "加入我们",
+            title: 'Community',
             items: [
               {
-                label: "Telegram群组",
-                href: "https://t.me/hezu1"
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
-                label: "Telegram频道",
-                href: "https://t.me/hezu2"
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
               },
               {
-                label: "教程频道",
-                href: "https://t.me/godlynews1"
-              }
-            ]
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
           },
           {
-            title: "帮助我们美化页面",
+            title: 'More',
             items: [
               {
-                label: "GitHub",
-                href: "https://github.com/shadowsside/hezu-wiki"
-              }
-            ]
-          }
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} 电报第一合租群, @hezu1. Built with Docusaurus.`
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      }
-    })
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
