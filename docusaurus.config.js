58d34f69c172f08f499fbd517c7be55793cf9398
ShadowsSide <61958350+ShadowsSide@users.noreply.github.com>
2022-05-24 13:03:10 +0800

    Edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,15 +35,16 @@ module.exports = ({
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+            "https://github.com/ShadowsSide/hezu_wiki/tree/master/"
         },
+        /** 
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        },
+        },*/
         theme: {
           customCss: require.resolve("./src/css/custom.css")
         }
